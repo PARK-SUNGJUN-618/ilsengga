4acf77694094ff5fad617530e5fe7b0e1c9d9c40
Sungjun Park <espoir618@gmail.com>
2026-03-04 19:05:37 +0900

    ilsengga-Add main page style

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-    <main>
-      <h1>일생가</h1>
-      <p>일본 생활 가능하세요?</p>
-      <p>Japan Life, Simplified.</p>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">일생가</h1>
+
+      <p className="mt-4 text-2xl">일본 생활 가능하세요?</p>
+
+      <p className="mt-2 text-gray-500">Japan Life, Simplified.</p>
     </main>
   );
 }
