6946fc5533bf98d9d5197a1c96a07192d4a7c952
Sungjun Park <espoir618@gmail.com>
2026-03-13 19:57:19 +0900

    ilsengga-Add sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ilsengga.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ilsengga.vercel.app/tools/job-change",
+      lastModified: new Date(),
+    },
+  ];
+}
