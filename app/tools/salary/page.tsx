dac7e43bc9777ff4ae86bf664a9a19629235ade6
Sungjun Park <espoir618@gmail.com>
2026-03-16 21:43:10 +0900

    ilsengga-Add metadata of salary

---
+++ app/tools/salary/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "일본 월급 실수령액 계산기",
+  description:
+    "일본 월급을 입력하면 건강보험, 후생연금, 고용보험, 소득세, 주민세 등을 고려해 예상 실수령액을 계산해보세요.",
+  keywords: [
+    "일본 월급 실수령액",
+    "일본 월급 계산기",
+    "일본 실수령액 계산기",
+    "일본 연봉 실수령액",
+    "일본 월급 세금",
+    "일본 직장인 실수령액",
+  ],
+};
