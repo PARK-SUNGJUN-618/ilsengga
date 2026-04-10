69fd88e3c837a3c61c8e8470a911b8c27634f804
Sungjun Park <espoir618@gmail.com>
2026-04-10 19:07:16 +0900

    ilsengga-edit sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,12 +6,24 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://ilsengga.vercel.app",
       lastModified: new Date(),
     },
+
+    // 돈 / 직장
+    {
+      url: "https://ilsengga.vercel.app/tools/salary",
+      lastModified: new Date(),
+    },
     {
       url: "https://ilsengga.vercel.app/tools/job-change",
       lastModified: new Date(),
     },
+
+    // 메뉴 도감
     {
-      url: "https://ilsengga.vercel.app/tools/salary",
+      url: "https://ilsengga.vercel.app/food",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://ilsengga.vercel.app/food/sushi",
       lastModified: new Date(),
     },
   ];
